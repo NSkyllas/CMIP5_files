86c7f390d69c272ca88d40790445294876e0cbcb
Nomikos <62618905+NSkyllas@users.noreply.github.com>
2021-06-15 17:37:13 +0200

    Update CMIP5.py

--- CMIP5.py
+++ CMIP5.py
@@ -43,11 +43,11 @@ if len(f_data3)>0:
 	st.write(f_data3)
 	st.write(str(len(f_data3)) + " files with " + str(len(f_data3["model"].unique())) + " models")
 	##########################
-	csv = f_data3.to_csv(index=False)
-	b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-	href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-	st.markdown("## Download the table:")
-	st.markdown(href, unsafe_allow_html=True)	
+	#csv = f_data3.to_csv(index=False)
+	#b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+	#href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+	#st.markdown("## Download the table:")
+	#st.markdown(href, unsafe_allow_html=True)	
 	###########################
 elif len(f_data2)>0:
 	st.write(f_data2)
